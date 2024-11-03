3d72430057ed84fd47b232b871a642fa50208ed9
Akarsh Singh <akarshs641@gmail.com>
2024-11-03 18:10:49 +0530

    Added Layout Sidebar and Mobile Navigation

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,7 +1,6 @@
-"use client";
-
+ "use client";
+import { Sheet, SheetContent, SheetTrigger, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import { Button } from "../ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { navLinks } from "@/constants";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { UserButton } from "@clerk/nextjs";
@@ -20,9 +19,9 @@ const MobileNav = () => {
           alt="logo"
           width={180}
           height={28}
-        ></Image>
+        />
       </Link>
-      <nav className="flex  gap-2">
+      <nav className="flex gap-2">
         <SignedIn>
           <UserButton />
           <Sheet>
@@ -33,48 +32,50 @@ const MobileNav = () => {
                 width={32}
                 height={32}
                 className="cursor-pointer"
-              ></Image>
+              />
             </SheetTrigger>
             <SheetContent className="sheet-content sm:w-64">
-              <>
-                <Image
-                  src="/assets/images/logo-text.svg"
-                  alt="logo"
-                  width={152}
-                  height={23}
-                ></Image>
-                <ul className="header-nav_elements">
-                  {navLinks.map((link) => {
-                    const isActive = link.route === pathname;
-                    return (
-                      <li
-                        key={link.route}
-                        className={`${
-                          isActive && "gradient-text"
-                        } p-18 flex whitespace-nowrap  text-dark-700 `}
+              <SheetHeader>
+                
+                <SheetTitle></SheetTitle>
+              </SheetHeader>
+              <Image
+                src="/assets/images/logo-text.svg"
+                alt="logo"
+                width={152}
+                height={23}
+              />
+              <ul className="header-nav_elements">
+                {navLinks.map((link) => {
+                  const isActive = link.route === pathname;
+                  return (
+                    <li
+                      key={link.route}
+                      className={`${
+                        isActive && "gradient-text"
+                      } p-18 flex whitespace-nowrap text-dark-700`}
+                    >
+                      <Link
+                        className="sidebar-link cursor-pointer"
+                        href={link.route}
                       >
-                        <Link
-                          className="sidebar-link cursor-pointer"
-                          href={link.route}
-                        >
-                          <Image
-                            src={link.icon}
-                            alt="logo"
-                            width={24}
-                            height={24}
-                          ></Image>
-                          {link.label}
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </>
+                        <Image
+                          src={link.icon}
+                          alt={link.label}
+                          width={24}
+                          height={24}
+                        />
+                        {link.label}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
             </SheetContent>
           </Sheet>
         </SignedIn>
         <SignedOut>
-          <Button asChild className="button bg-purple-gradient bg-cover ">
+          <Button asChild className="button bg-purple-gradient bg-cover">
             <Link href="/sign-in">Login</Link>
           </Button>
         </SignedOut>
