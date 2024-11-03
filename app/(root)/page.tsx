50118cba13e57740656078d975bfc438c7976a1a
Akarsh Singh <akarshs641@gmail.com>
2024-11-03 17:48:43 +0530

    Added Layout Sidebar and Mobile Navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { UserButton } from '@clerk/nextjs'
+
 
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-      <UserButton />
+      
     </div>
   )
 }
